f5b669e7c957bfd277b24611d28ab6a473c1b5a7
nharper0792 <nharper0792@gmail.com>
2023-02-24 09:37:38 -0500

    created proc1-5

---
+++ kernel/processes.c
@@ -0,0 +1,27 @@
+#include <processes.h>
+
+void proc1(){
+    puts("Hello from proc1\n");
+}
+void proc2(){
+    puts("Hello from proc2\n");
+    puts("Hello from proc2\n");
+}
+void proc3(){
+    puts("Hello from proc3\n");
+    puts("Hello from proc3\n");
+    puts("Hello from proc3\n");
+}
+void proc4(){
+    puts("Hello from proc4\n");
+    puts("Hello from proc4\n");
+    puts("Hello from proc4\n");
+    puts("Hello from proc4\n");
+}
+void proc5(){
+    puts("Hello from proc5\n");
+    puts("Hello from proc5\n");
+    puts("Hello from proc5\n");
+    puts("Hello from proc5\n");
+    puts("Hello from proc5\n");
+}
