2fd6b772e8af3efdf81f9950461cdee8ce6b593b
ebbalot <epw0010@mix.wvu.edu>
2023-03-24 09:03:51 -0400

    R5 - implemented prototype memory control blocks

--- Makefile
+++ Makefile
@@ -9,7 +9,8 @@ kernel/kmain.o\
 kernel/serial.o\
 kernel/pcb.o\
 kernel/sys_call.o\
-kernel/command_history.o
+kernel/command_history.o\
+kernel/heap_management.o
 
 LIB_OBJECTS =\
 lib/ctype.o\
--- include/heap_management.h
+++ include/heap_management.h
@@ -1,6 +1,23 @@
 #ifndef MPX_HEAP_H
 #define MPX_HEAP_H
 
+#include <linked-list.h>
+
+enum mem_flag {
+	FREE = 0,
+	ALLOCATED = 1
+};
+
+typedef struct memb {
+	size_t start_address;
+	size_t size;
+	struct memb* nextPtr;
+	struct memb* prevPtr;
+}memb;
+
+extern list* memb_freeList;
+extern list* memb_allocList;
+
 void initialize_heap(size_t data);
 
 int free_memory(void* data);
