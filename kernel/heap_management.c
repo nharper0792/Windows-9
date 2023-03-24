aad56e057c5745c40459e58dfdad99c818a22fc5
ebbalot <epw0010@mix.wvu.edu>
2023-03-24 10:22:33 -0400

    R5 - finished initialize_heap()

--- kernel/heap_management.c
+++ kernel/heap_management.c
@@ -6,14 +6,16 @@ mcbList mcb_freeList;
 mcbList mcb_allocList;
 
 void initialize_heap(size_t size) {
+	//make memory control block for free List
 	mcb* newMemb = (mcb*)sys_alloc_mem(sizeof(mcb));
+	//create starting memory location
 	size_t startMem = (size_t)(kmalloc(size + sizeof(mcb), 0, NULL));
-
+	//set parameters of free memory
 	newMemb->start_address = startMem;
 	newMemb->size = size;
 	newMemb->nextPtr = NULL;
 	newMemb->prevPtr = NULL;
-
+	//initialize lists
 	mcb_freeList.headPtr = newMemb;
 	mcb_allocList.headPtr = NULL;
 
