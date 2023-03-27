9d407b65bc45a6eaa5bc950bebcd99dd16285643
01ethanwright <01ethanwright@gmail.com>
2023-03-27 09:40:11 -0400

    R5: started working on allocate_memory

--- kernel/heap_management.c
+++ kernel/heap_management.c
@@ -27,6 +27,28 @@ int free_memory(void* data) {
 }
 
 void* allocate_memory(size_t data) {
-	(void)data;
-	return 0;
+	//traversing list to find location free mcb with enough space
+	mcb* currentPtr;
+	for (currentPtr = mcb_List.headPtr; (currentPtr->size >= data && currentPtr->flag == FREE) || currentPtr == NULL; currentPtr = currentPtr->nextPtr);
+
+	//checking to see if location was found
+	if (currentPtr->size >= data && currentPtr->flag == FREE) {
+		size_t newBlockSize = currentPtr->size - data;
+		
+		//changes free block to allocated block
+		currentPtr->flag = ALLOCATED;
+		currentPtr->size = data;
+
+		//creates new block and adds to list after new alloc block
+		mcb* newBlock = sys_alloc_mem(sizeof(mcb));
+		newBlock->flag = FREE;
+		newBlock->size = newBlockSize;
+		//get start address for new memory block
+
+		//allocates memory in system
+		//returns pointer for new allocated memory
+	}
+
+	//fallthrough (could not find free mcb block in list that will fit)
+	return NULL;
 }
