7d9eb8ab721382b9899a82cc64a7d8e60b87c054
tsm0029 <tsm0029@mix.wvu.edu>
2023-04-14 10:33:54 -0400

    serial_open

--- kernel/ioscheduler.c
+++ kernel/ioscheduler.c
@@ -2,6 +2,13 @@
 #include <serial.h>
 #include <io.h>
 
+
+#define eflagO 1
+#define eflagC 2
+#define inUSE 3
+#define EOImask 0x20
+#define PICmask 0x21
+
 enum uart_registers {
     RBR = 0,	// Receive Buffer
     THR = 0,	// Transmitter Holding
@@ -21,23 +28,27 @@ static int initialized[4] = { 0 };
 iocb* iocbHead;
 int serial_open(device dev, int speed)
 {
+	dcb* DCB = (dcb*)DCB;
+	DCB->event_status = eflagO;
+	DCB->use_status = IDLE;
 	int dno = serial_devno(dev);
     if (dno == -1) {
         return -1;
     }
-	dcb* DCB = (dcb*)DCB;
-	DCB->event_status = 0;
-	DCB->use_status = IDLE;
     outb(dev + IER, 0x00);	//disable interrupts
     outb(dev + LCR, 0x80);	//set line control register
-    outb(dev + DLL, 115200 / 9600);	//set bsd least sig bit
+    outb(dev + DLL, 115200 / 9600);	//set bsd least sig bit check whether baud rate is right
     outb(dev + DLM, 0x00);	//brd most significant bit
     outb(dev + LCR, 0x03);	//lock divisor; 8bits, no parity, one stop
-    outb(dev + FCR, 0xC7);	//enable fifo, clear, 14byte threshold
-    outb(dev + MCR, 0x0B);	//enable interrupts, rts/dsr set
-    (void)inb(dev);		//read bit to reset port
+    outb(dev + FCR, 0xC7);	//enable fifo, clear, 14byte thresholdt
+	outb(dev+1,0x01);
     initialized[dno] = 1;
-
+	int mask;
+	mask = inb(PICmask);
+	mask = mask&~10;
+	outb(PICmask,mask);
+	outb(dev + MCR, 0x0B);	//enable interrupts, rts/dsr set
+    (void)inb(dev);		//read bit to reset port
 	return 0;
 }
 
